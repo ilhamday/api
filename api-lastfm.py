a54a485c5c877b5c22aecbe38936f46d158cf483
ilhamday <ilhamyudha00@gmail.com>
2021-03-09 22:58:43 +0700

    Update - print json result using function

--- api-lastfm.py
+++ api-lastfm.py
@@ -18,19 +18,46 @@ import json
 #
 # print(r.status_code)
 
-# Function
+# My Function --------------
+# def lastfm_get(payload):
+#     # define headers and URL
+#     headers = {'user-agent': keyLastfm.USER_AGENT}
+#
+#     params = {
+#         'api_key': keyLastfm.API_KEY,
+#         'format': 'json',
+#         'method': payload,
+#     }
+#
+#     response = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=params)
+#
+#     return response
+#
+# r = lastfm_get('chart.gettopartists')
+# ---------------------------
+
 def lastfm_get(payload):
     # define headers and URL
     headers = {'user-agent': keyLastfm.USER_AGENT}
+    url = 'http://ws.audioscrobbler.com/2.0/'
 
-    params = {
-        'api_key': keyLastfm.API_KEY,
-        'format': 'json',
-        'method': payload,
-    }
+    # Add API ket and format to the payload
+    payload['api_key'] = keyLastfm.API_KEY
+    payload['format'] = 'json'
 
-    r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=payload)
+    return response
 
-    print(r.status_code)
+r = lastfm_get({
+    'method': 'chart.gettopartists'
+})
 
-lastfm_get('chart.gettopartists')
+print(r.status_code)
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+# jprint(r.json())
+jprint(r.json()['artists']['@attr'])
