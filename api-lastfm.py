f3426cd931c9dac7fa59d5ccfc29015dddd8c7a0
ilhamday <ilhamyudha00@gmail.com>
2021-03-09 07:32:50 +0700

    Update with function

--- api-lastfm.py
+++ api-lastfm.py
@@ -2,18 +2,35 @@ import keyLastfm
 import requests
 import json
 
-headers = {
-    'user-agent': keyLastfm.USER_AGENT
-}
+# headers = {
+#     'user-agent': keyLastfm.USER_AGENT
+# }
+#
+# payload = {
+#     'api_key': keyLastfm.API_KEY,
+#     'method': 'chart.gettopartists',
+#     'format': 'json'
+# }
+#
+# #  /2.0/?method=chart.gettopartists&api_key=YOUR_API_KEY&format=json
+#
+# r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+#
+# print(r.status_code)
 
-payload = {
-    'api_key': keyLastfm.API_KEy,
-    'method': 'chart.gettopartists',
-    'format': 'json'
-}
+# Function
+def lastfm_get(payload):
+    # define headers and URL
+    headers = {'user-agent': keyLastfm.USER_AGENT}
 
-#  /2.0/?method=chart.gettopartists&api_key=YOUR_API_KEY&format=json
+    params = {
+        'api_key': keyLastfm.API_KEY,
+        'format': 'json',
+        'method': payload,
+    }
 
-r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+    r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=params)
 
-print(r.status_code)
+    print(r.status_code)
+
+lastfm_get('chart.gettopartists')
